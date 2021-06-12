df406cada1a022e51737cfe3e5e905e7286c0f1c
casparsylwan <casparsylwan@gmail.com>
2021-06-12 21:12:22 +0200

    aktive route

--- src/app/navigation/cover-navigation/cover-navigation.component.ts
+++ src/app/navigation/cover-navigation/cover-navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-cover-navigation',
@@ -11,9 +12,19 @@ export class CoverNavigationComponent implements OnInit {
   timeoutOpen:boolean = false;
   openClass = 'close';
 
-  constructor() { }
+  activeRoute:string = '';
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    console.log(this.router.url)
+    this.router.events.subscribe((val) => {
+      if(val instanceof NavigationEnd)
+      {
+        this.activeRoute = val.url;
+      }
+      
+    });
   }
 
   openClose(event)
