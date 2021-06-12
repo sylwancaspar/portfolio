ea49d1ed6270c229740277c40199d207bf90237c
casparsylwan <casparsylwan@gmail.com>
2021-06-12 20:21:39 +0200

    redirect on ''

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
-  {path:'', component: HomeComponent},
+  {path:'', redirectTo: 'home', pathMatch: 'full'},
   {path:'home', component: HomeComponent},
   {path:'projects', component: HomeComponent},
   {path:'about', component: HomeComponent},
