adb23ed9ee87018d6e36b7b10650324d0fa124a4
casparsylwan <casparsylwan@gmail.com>
2021-06-12 20:43:32 +0200

    behavioralSubject _language

--- src/app/service/state.service.ts
+++ src/app/service/state.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StateService {
+
+  private _language:Observable<string> = new BehaviorSubject<string>('sv');
 
   constructor() { }
 }
