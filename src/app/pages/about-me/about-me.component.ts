580a063ab07028f985b277f914eca5f1dcded145
casparsylwan <casparsylwan@gmail.com>
2021-06-14 19:33:47 +0200

    closing the menu on navigation

--- src/app/pages/about-me/about-me.component.ts
+++ src/app/pages/about-me/about-me.component.ts
@@ -19,7 +19,7 @@ export class AboutMeComponent implements OnInit {
   [
     {
       sprak: 'Java',
-      ramverk: ['Springboot', 'Hibernate/JPA', 'Thymeleaf', 'Java EE', 'JavaFX', 'Selenium']      
+      ramverk: ['Springboot', 'Hibernate/JPA', 'Thymeleaf', 'Java EE', 'JavaFX', 'Selenium (e2e)', 'Junit 5 (Unit testing)', "Mockito  (Unit testing)"]      
     },
     {
      sprak: 'Python',
@@ -30,19 +30,28 @@ export class AboutMeComponent implements OnInit {
       ramverk:['R-studio']
     },
     {
+      sprak:'TypeScript',
+      ramverk:['Leaflet', 'Chart.js']
+    },
+    {
       sprak:'Frontend bas',
-      ramverk:['HTML5', 'CSS', 'SCSS/SASS', 'AJAX', 'Web Content Accessibility Guidelines (WCAG)']
+      ramverk:['HTML5', 'CSS', 'SCSS/SASS', 'AJAX', 'Web Content Accessibility Guidelines (WCAG)', 'Bootstrap']
     }
     ,
     {
       sprak:'Frontend ramverk',
       ramverk:['Angular 2+', 'Rxjs', 'Protractor (e2e)', 'Jasmine/Karma (Unit testing)', 'Vue', 'VueX']
 
+    },
+    {
+      sprak:'Git',
+      ramverk:['']
     }
   ]
 
   workExperience:{heading:string,
                   type:string,
+                  place?:string,
                   from:Date,
                   to:Date,
                   description:string}[]=
@@ -97,7 +106,43 @@ export class AboutMeComponent implements OnInit {
                       from: new Date('1999-01-01'),
                       to: new Date('2008-01-01'),
                       description:"Egen företagare och frilansartist Producera, budgetera och marknadsföra konstnärliga event. Medverkade i produktioner som Melodiefestivalen, Super cirkör och flera andra."
+                    },
+                    {
+                      heading: "Yrkeshögskoleexamen Javautvecklare",
+                      place:"IT-högskolan",
+                      type: 'education',
+                      from: new Date('2019-01-01'),
+                      to: new Date('2021-01-01'),
+                      description: "Fullstack, både back-end och front-end."
+                    },
+                    {
+                      heading: "Kandidatexamen i Nationalekonomi",
+                      place: "Stockholms Universitet",
+                      type: 'education',
+                      from: new Date('2019-01-01'),
+                      to: new Date('2021-01-01'),
+                      description: "Inkluderande 60 hp i matematik och statistik. Dessa 60hp inkluderade flera kurser i linjäralgebra, envariabelsanalys, flervariabelsanalys, regressionsanalys, Multivariata metoder, Avancerad programmering i R."
+                    },
+                    {
+                      heading: "Konstnärlig kandidatexamen i scenkonst",
+                      place:"BELGIEN",
+                      type: 'education',
+                      from: new Date('2019-01-01'),
+                      to: new Date('2021-01-01'),
+                      description: "L’Ecole Supérieure des Arts du Cirque – ESAC"
                     }
                   ]
+
+  giveWork()
+  {
+    return this.workExperience
+      .filter(work => work.type == 'work')
+  }
+
+  giveEducation()
+  {
+    return this.workExperience
+      .filter(work => work.type == 'education')
+  }
 
 }
